cf0ab8f4cd0ce4a90d16473931db624b7bd234bb
pareshsahoo902 <pareshsahoo902@gmail.com>
2021-02-04 16:45:53 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import './VideoChatContainer';
+import VideoChatContainer from './VideoChatContainer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>WebRTC P2PVideoChat</h1>
+      <h2>React + Firebase</h2>
+      <VideoChatContainer />
     </div>
   );
 }
 
-export default App;
+export default App
---
+++ src/config.js
@@ -0,0 +1,12 @@
+const config = {
+    apiKey: "AIzaSyBwkKLydAfM1jcw4VWB2GbLjsTXkfAlfkU",
+    authDomain: "webrtcapp-50ea6.firebaseapp.com",
+    databaseURL: "https://webrtcapp-50ea6-default-rtdb.firebaseio.com",
+    projectId: "webrtcapp-50ea6",
+    storageBucket: "webrtcapp-50ea6.appspot.com",
+    messagingSenderId: "93942797139",
+    appId: "1:93942797139:web:1979e87288a0308f5a47a6"
+  }
+  
+  export default config
+  
